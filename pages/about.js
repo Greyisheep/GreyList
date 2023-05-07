db67fc5eb2c0972e456e3430440f03519757ad74
Greyisheep <97015429+Greyisheep@users.noreply.github.com>
2023-05-07 12:44:01 +0100

    initial

---
+++ pages/about.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor officia
+          voluptas ratione, rerum aut adipisci omnis dolorum nulla temporibus
+          aliquam! Enim sequi impedit quam at natus saepe obcaecati ipsum
+          distinctio.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor officia
+          voluptas ratione, rerum aut adipisci omnis dolorum nulla temporibus
+          aliquam! Enim sequi impedit quam at natus saepe obcaecati ipsum
+          distinctio.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
